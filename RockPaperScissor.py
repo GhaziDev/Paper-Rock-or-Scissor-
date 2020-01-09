6b1c9ff4c41149f698f49aaf3070b4ed4afb3997
ghazigamer <46258765+ghazigamer@users.noreply.github.com>
2020-01-09 23:18:16 +0200

    Update RockPaperScissor.py

--- RockPaperScissor.py
+++ RockPaperScissor.py
@@ -1,28 +1,31 @@
 import random
+
 choice = ["paper", "scissor", "rock"]
-tie=0
-win=0
-lose=0
+tie = 0
+win = 0
+lose = 0
 while True:
     q = str(input("Paper,Scissor,or Rock?  ")).lower()
-    c=random.choice(choice)#good info...here it get overwritten everytime the loop start over
+    c = random.choice(choice)  # good info...here it get overwritten everytime the loop start over
     while (q != "paper" and q != "scissor" and q != "rock"):
         q = str(input("Paper,Scissor,or Rock?  ")).lower()
     if (q == c):
         print(c)
         print("Draw!")
-        tie=tie+1
-    elif ((q == "paper" and c == "rock") or (q == "scissor" and c == "paper") or (q == "rock" and c == "scissor")):
+        tie = tie + 1
+    elif ((q == "paper" and c == "rock")
+        or (q == "scissor" and c == "paper")
+        or (q == "rock" and c == "scissor")):
         print(c)
         print("You won")
-        win=win+1
+        win = win + 1
     else:
         print(c)
         print("you lost")
-        lose=lose+1
+        lose = lose + 1
     print(f"Wins : {win}\nLoses : {lose}\nTies : {tie}")
-    p=int(input("Do you want to play again...enter 1 for 'Yes', otherwise  for 'No'?  "))
-    if(p==1):
+    p = int(input("Do you want to play again...enter 1 for 'Yes', otherwise  for 'No'?  "))
+    if (p == 1):
         continue
     else:
         print("Game ended")
